f74cf3377ae62ba73471667da0be0934103f9fa7
Arthur Liu <arthur.liu.lsh@protonmail.com>
2023-12-04 06:21:27 +0100

    refactor: remove useless dict in d4

--- d4.py
+++ d4.py
@@ -3,26 +3,6 @@ import re
 
 lines = utils.parse("d4.txt")
 
-convert = {'one':'1',
-           'two':'2',
-           'three':'3',
-           'four':'4',
-           'five':'5',
-           'six':'6',
-           'seven':'7',
-           'eight':'8',
-           'nine':'9',
-           '1':'1',
-           '2':'2',
-           '3':'3',
-           '4':'4',
-           '5':'5',
-           '6':'6',
-           '7':'7',
-           '8':'8',
-           '9':'9'
-           }
-
 @utils.measure
 def run():
     sum1 = 0
